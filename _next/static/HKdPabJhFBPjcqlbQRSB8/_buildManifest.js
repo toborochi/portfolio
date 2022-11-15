8ccacf5ff61f02474260542b72ceee5dfc5eb6eb
toborochi <nozosimo@gmail.com>
2022-11-15 05:15:43 +0000

    Deploying to gh-pages from @ toborochi/portfolio@9c5eeebc1164df9121ced3757acca778e3725bfb 🚀

--- _next/static/HKdPabJhFBPjcqlbQRSB8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/37874e6db66c398c.css","static/chunks/pages/index-f91e2524ede9e9ab.js"],"/_error":["static/chunks/pages/_error-8e848193de3c139c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
