c061eb54975a2f1a7b8b30c85a736afb0c79885f
Toborochi <nozosimo@gmail.com>
2022-11-19 10:40:09 -0400

    wip

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    loader: "akamai",
-    path: ""
-  },
-  basePath: "/portfolio",
-  assetPrefix:  "/portfolio",
+  swcMinify: true
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -21,21 +21,21 @@ export default function Home() {
       </Head>
       <header className={styles.header_portfolio}>
         
-        <section className={styles.language_box}>
+        <section className={styles.menu_box}>
           <button>about</button>
           <button>projects</button>
           <button>contact</button>
         </section>
         
         <section className={styles.language_box}>
-          <div className={styles["circular--portrait"]}> <Image alt=""  src={`${process.env.FAVICON}/us.svg`}/> </div>
-          <div className={styles["circular--portrait"]}> <Image alt=""  src={`${process.env.FAVICON}/bo.svg`} /> </div>
+          <div className={styles["circular--portrait"]}> <Image alt=""  src={`/us.svg`} width={500} height={500} /> </div>
+          <div className={styles["circular--portrait"]}> <Image alt=""  src={`/bo.svg`}  width={500} height={500} /> </div>
         </section>
       </header>
 
       <footer className={styles.footer_portfolio }>
-      <a href="https://github.com/toborochi" target="_blank" rel="noreferrer" > <Image alt="" className={styles.footer_icon}  src={`${process.env.FAVICON}/github.svg`}/> </a>
-      <a href="https://www.linkedin.com/in/leonardoav/" target="_blank" rel="noreferrer"> <Image alt="" className={styles.footer_icon} src={`${process.env.FAVICON}/icons8-linkedin-50.svg`}/> </a>
+      <a href="https://github.com/toborochi" target="_blank" rel="noreferrer" > <Image alt="" className={styles.footer_icon}  src={`/github.svg`}  width={500} height={500}/> </a>
+      <a href="https://www.linkedin.com/in/leonardoav/" target="_blank" rel="noreferrer"> <Image alt="" className={styles.footer_icon} src={`/icons8-linkedin-50.svg`}  width={500} height={500}/> </a>
       </footer>
 
       <div className={styles.container_name}>
